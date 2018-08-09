08a24fdaeb7aaac4a3170b2b44068da5ddd52de0
Harshal Kulkarni <harshalkulkarni@Harshals-MacBook-Pro.local>
2018-08-09 08:46:04 -0700

    Modified for using nginx

--- tiny-url/src/main/java/com/tinyurl/controller/TinyUrlController.java
+++ tiny-url/src/main/java/com/tinyurl/controller/TinyUrlController.java
@@ -26,30 +26,57 @@ public class TinyUrlController {
 	UserRepository userRepository;
 	@Autowired
 	URLRepository urlRepository;
+	private MessageDigest digest = null;
 
-	@PostMapping(value="/api/shortenUrl")
+	public TinyUrlController() {
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+			
+		}
+	}
+
+
+	@PostMapping(value="/api/v1/shortenUrl")
 	public String shortenUrl(@RequestBody URL request) {
 		User user = userRepository.findByUserId(request.getUserId());
 		if (user == null) {
 			return "User doesn't exist";
 		}
 		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] encodedhash = digest.digest(
-					request.getOriginalUrl().getBytes(StandardCharsets.UTF_8));
-			String encodedHash = Base64.getUrlEncoder().encodeToString(encodedhash);
-			String shaString = new String(encodedHash.substring(0, 10));
-			request.setHashKey(shaString);
+			String origUrl = request.getOriginalUrl();
+			String hashKey = findShortenedUrl(origUrl, request.getUserId());
+
+			request.setHashKey(hashKey);
 			request.setCreationDate(LocalDateTime.now());
 			urlRepository.save(request);
-			return "Shortedn URL created=>"+shaString;
+			return "Shortedn URL created=>"+hashKey;
 		}catch(Exception e) {
 			e.printStackTrace();
 			return "Failed to generate shortened URL";
 		}
 
 	}
-	
+
+	private String findShortenedUrl(String origUrl, int userId) {
+		String hashKey = null;
+		while(hashKey == null) {
+			byte[] encodedhash = digest.digest(
+					origUrl.getBytes(StandardCharsets.UTF_8));
+			String encodedHash = Base64.getUrlEncoder().encodeToString(encodedhash);
+			String shaString = new String(encodedHash.substring(0, 10));
+			URL url = urlRepository.findByHashKey(shaString);
+			if (url == null || (userId == url.getUserId() && origUrl.equals(url.getOriginalUrl()))) {
+				hashKey = shaString;
+			}else {
+				origUrl = origUrl+""+Math.random();
+			}
+		}
+		return hashKey;
+	}
+
 	@PostMapping(value="/api/deleteUrl")
 	public String deleteUrl(@RequestBody URL request) {
 		if (StringUtils.isEmpty(request.hashKey)) {
@@ -64,19 +91,23 @@ public class TinyUrlController {
 			return "Shortened URL deleted";
 		else 
 			return "Shortened URL not found";
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 	@GetMapping(value= "{id}")
 	public RedirectView redirect(@PathVariable String id) {
 		RedirectView redirectView = new RedirectView();
 		URL url = urlRepository.findByHashKey(id);
-	    redirectView.setUrl(url.getOriginalUrl());
-	    return redirectView;
-		
+		if (url != null) {
+			redirectView.setUrl(url.getOriginalUrl());
+			return redirectView;
+		}else {
+			return  null;
+		}
+
 	}
 
 
